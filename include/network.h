26e953107f356cdc0a8eeda335bb9b1bbc7214f3
Alex Clevenger <abc324@lehigh.edu>
2024-06-07 13:50:57 -0400

    file structure

---
+++ include/network.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "debug.h"
+
+/* Establish network-layer TCP connection */
+void prologue() {
+
+}
+
+/* Close network-layer TCP connection */
+void epilogue() {
+
+}
+
+/* Route messages to respective node */
+void msg_router(){
+
+}
+
+/* Serialize messaage using Marshal send to different peers */
+void msg_serialize(){
+
+}
+
+
+
+
+
